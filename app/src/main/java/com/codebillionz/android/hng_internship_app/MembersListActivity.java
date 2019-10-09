12a8150b9d4b6d5019a23a9b9cc481f0bee33a54
Stephen Oyebanji <seyioyebanjistephen@gmail.com>
2019-10-09 23:05:01 +0100

    members list activity is up

--- app/src/main/java/com/codebillionz/android/hng_internship_app/MembersListActivity.java
+++ app/src/main/java/com/codebillionz/android/hng_internship_app/MembersListActivity.java
@@ -1,8 +1,16 @@
 package com.codebillionz.android.hng_internship_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.codebillionz.android.hng_internship_app.Adapters.MembersAdapter;
+import com.codebillionz.android.hng_internship_app.Models.Members;
+
+import java.util.ArrayList;
 
 public class MembersListActivity extends AppCompatActivity {
 
@@ -10,5 +18,40 @@ public class MembersListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members_list);
+
+        final ArrayList<Members> members = new ArrayList<>();
+        RecyclerView memberRecycler = findViewById(R.id.members_recycler);
+        MembersAdapter adapter = new MembersAdapter(members, this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        memberRecycler.setLayoutManager(mLayoutManager);
+        memberRecycler.setItemAnimator(new DefaultItemAnimator());
+        memberRecycler.setAdapter(adapter);
+
+        // Create a list of members
+
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        members.add(new Members("https://drive.google.com/open?id=1DbWQs35dHg1RjFnckZyC-kZHpLBq00bS",
+                "HNG Internship 2019", "08099993939"));
+        adapter.notifyDataSetChanged();
+
     }
 }
